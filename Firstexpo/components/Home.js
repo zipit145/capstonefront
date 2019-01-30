30b60592ca495502a8d6c45d7aca4d0256e4a441
zipit145 <gregbforney@gmail.com>
2019-01-30 14:39:01 -0700

    front end stuff has been finalized

--- Firstexpo/components/Home.js
+++ Firstexpo/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -9,10 +9,16 @@ class HomeScreen extends React.Component {
     };
     render() {
         return (
-    <View style={styles.container}>
-        <TextInput style={styles.inp}>Username</TextInput>
-        <Button title="Go to Details" onPress={() => this.props.navigation.navigate('Search')} />
-    </View>
+        <View style={styles.container}>
+            <TextInput style={styles.inp}>Username</TextInput>
+            <TouchableOpacity
+            style={styles.butt}
+            onPress={() => this.props.navigation.navigate('Search')}
+        >
+            <Text style={styles.textg}> Touch Here </Text>
+        </TouchableOpacity>
+        </View>
+    
           );
     }
   }
@@ -20,17 +26,41 @@ class HomeScreen extends React.Component {
   const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ADD8E6',
+        backgroundColor: '#000',
         alignItems: 'center',
         justifyContent: 'center',
+        fontSize: 24,
     },
     inp: {
         textAlign: 'center',
-        backgroundColor: '#FFF',
-        marginBottom: 10,
-        minWidth: 150,
-        borderRadius: 6,
+        backgroundColor: '#76cffb',
+        borderColor: '#F1E3F3',
+        marginBottom: 300,
+        marginTop: 200,
+        minWidth: 400,
+        height: 100,
+        borderRadius:10,
         borderWidth: 1,
-    }
+        fontSize: 24,
+    },
+    textg: {
+        paddingTop: 50,
+        fontSize: 24,
+        textAlign: 'center',
+    },
+    butt:{
+        marginRight:40,
+        marginLeft:40,
+        marginTop:10,
+        paddingTop:10,
+        minWidth: 150,
+        height: 150,
+        paddingBottom:10,
+        backgroundColor:'#ffb347',
+        borderRadius:100,
+        borderWidth: 1,
+        borderColor: '#F1E3F3',
+        fontSize: 24,
+      },
   });
   export default HomeScreen
--- Firstexpo/components/Search.js
+++ Firstexpo/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 
 class Search extends React.Component {
@@ -9,31 +9,77 @@ class Search extends React.Component {
     render() {
       return (
         <View style={styles.container}>
-          <Text>Please Enter a Search Term</Text>
-          <TextInput  style={styles.inp}>Development</TextInput>
-          <Button
-            title="Go to Camera"
-            onPress={() => this.props.navigation.navigate('Camera')}
-          />
+          <TextInput style={styles.inp}>Development</TextInput>
+          <TouchableOpacity
+          style={styles.butt}
+          onPress={() => this.props.navigation.navigate('Camera')}>
+          <Text style={styles.textg}>Go to Camera</Text>
+          </TouchableOpacity>
         </View>
       );
     }
 }
+
 const styles = StyleSheet.create({
   container: {
       flex: 1,
-      backgroundColor: '#ADD8E6',
+      backgroundColor: '#000',
       alignItems: 'center',
       justifyContent: 'center',
+      fontSize: 24,
   },
   inp: {
       textAlign: 'center',
-      backgroundColor: '#FFF',
-      marginBottom: 10,
-      minWidth: 150,
-      borderRadius: 4,
+      backgroundColor: '#76cffb',
+      borderColor: '#F1E3F3',
+      marginBottom: 300,
+      marginTop: 200,
+      minWidth: 400,
+      height: 100,
+      borderRadius:10,
       borderWidth: 1,
-  }
+      fontSize: 24,
+  },
+  textg: {
+      paddingTop: 50,
+      fontSize: 24,
+      textAlign: 'center',
+  },
+  butt:{
+      marginRight:40,
+      marginLeft:40,
+      marginTop:10,
+      paddingTop:10,
+      minWidth: 150,
+      height: 150,
+      paddingBottom:10,
+      backgroundColor:'#ffb347',
+      borderRadius:100,
+      borderWidth: 1,
+      borderColor: '#F1E3F3',
+      fontSize: 24,
+    },
 });
+// const styles = StyleSheet.create({
+//   container: {
+//       flex: 1,
+//       backgroundColor: '#ADD8E6',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//   },
+//   inp: {
+//       textAlign: 'center',
+//       backgroundColor: '#FFF',
+//       marginBottom: 10,
+//       minWidth: 150,
+//       borderRadius: 4,
+//       borderWidth: 1,
+//   },
+//   butt: {
+//       borderRadius: 6,
+//       borderWidth: 1,
+//       backgroundColor: '#000000',
+//   }
+// });
 
 export default Search
